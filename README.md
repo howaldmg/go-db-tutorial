c8f5d56675eb646074ada5c56e29e7ae2b621690
Michael Howald <howaldmg@gmail.com>
2023-03-27 22:11:32 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#  go-db-tutorial
+
+This is my verison of the tutorial found here: https://go.dev/doc/tutorial/database-access
+
+docker compose file and sql init included since tutorial didn't include.
---
+++ data-access/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func main() {
+	cfg := mysql.Config{
+		User:                 "root",
+		Passwd:               "example",
+		Net:                  "tcp",
+		Addr:                 "127.0.0.1:3306",
+		DBName:               "recordings",
+		AllowNativePasswords: true,
+	}
+
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
+	albums, err := albumsByArtist("John Coltrane")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found: %v\n", albums)
+}
